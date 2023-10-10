490a092a52dfcc73e4d9d0f6e891ce296a2dcfaa
Sheryoo <sherif.zayan5@gmail.com>
2023-10-10 16:46:25 +0300

    Change the response of update status to be JSON response

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,4 @@
-from urllib import response
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework import generics
 from .serializers import TodoSerializer
 from .models import Todo
@@ -37,9 +35,9 @@ class EditStatus(generics.UpdateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
-    def update(self, request):
+    def update(self, request, *args, **kwargs):
         instance = self.get_object()
         instance.completed = not request.data.get('completed', instance.completed)
         instance.save()
         serializer = self.get_serializer(instance)
-        return HttpResponse(serializer.data)
+        return JsonResponse(serializer.data)
